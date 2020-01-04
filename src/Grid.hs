588c1a424e90c6c778b47fdbf74a93bd79c2dd10
Daniel Tuveson <danieltuveson@gmail.com>
2020-01-04 14:14:35 -0800

    started adding tests, developed outline of primary solver function

--- src/Grid.hs
+++ src/Grid.hs
@@ -1,6 +1,15 @@
 module Grid where 
 
 import Data.Array
+import Data.List.Index(indexed)
+import Data.Maybe(isJust)
+
+
+type RowNum    = Int
+type ColNum    = Int 
+type Point     = (RowNum, ColNum)
+-- type BlockNum  = Int 
+type CellValue = Maybe Int 
 
 {- 
     - Used to describe the structure of a sudoku cell 
@@ -10,24 +19,73 @@ import Data.Array
     - Blocks will be similarly numbered 1-9 left to right, top to bottom
     - isInitial is true if the cell was created by the constructor, false otherwise
 -}
-data Cell = Cell {
-    contents :: Maybe Int, 
-    isInitial :: Bool,
-    block :: Int
-} deriving(Show)
+data Cell = Cell (Point, CellValue) deriving(Show)
 
-type Row = Int
-type Col = Int 
+
 
 -- Defines the game grid used for sudoku
-data Grid = Grid [[Cell]]
-    deriving(Show)
+type Grid = [Cell]
 
--- Takes a list of rows that wil define the inital state of our grid.
--- This assumes the input is well formed, i.e. a list of 9 lists of
--- ints between 1-9. 
-gridInit :: [[Int]] -> Grid 
-gridInit ls = Grid $ map initRow ls 
+instance Show Grid where 
+    show cell@(Cell ((_, ColNum), _))
+        | 
 
-initRow :: [Int] -> [Cell]
-initRow _ = [Cell { contents = Just 5, isInitial = True, block = 5 }]
+-- -- Takes a list of rows that wil define the inital state of our grid.
+-- -- This assumes the input is well formed, i.e. a list of 9 lists of
+-- -- ints between 1-9, with both rows and columns being indexed 0-9
+-- initGrid :: [(RowNum, [(ColNum, Maybe Int)])] -> Grid
+-- initGrid grid = Grid $ map initRow grid
+--     where initRow (i, row) = map (initCell i) row
+
+-- -- Initializes a given cell in our grid
+-- initCell :: RowNum -> (ColNum, Maybe Int) -> Cell
+-- initCell i (j, maybeNum) = 
+--     Cell {
+--         contents  = maybeNum, 
+--         isInitial = True, 
+--         block     = getBlock i j
+--     }
+
+-- Returns the block associated with a given cell's row & column
+getBlock :: RowNum -> ColNum -> BlockNum
+getBlock i j = 3 * (getSeg i - 1) + getSeg j 
+    where getSeg num
+            | elem num [1, 2, 3] = 1 
+            | elem num [4, 5, 6] = 2
+            | elem num [7, 8, 9] = 3
+
+-- getSeg :: ColNum -> Int
+-- getSeg num
+--     | elem num [1, 2, 3] = 1 
+--     | elem num [4, 5, 6] = 2
+--     | elem num [7, 8, 9] = 3
+
+data Seg = One | Two | Three 
+-- getBlock _ _ = error "this should really not happen lol"
+
+
+-- takes in state of the board as a list of (Row, Col, Maybe Value) 
+-- and a list of (Row, Col, Maybe Value) (with Maybe value being Nothing) to fill
+-- and returns the solution if it is solvable 
+solve :: Grid -> [(RowNum, ColNum)] -> Int -> Maybe Grid
+solve _ _ num | num > 9      = Nothing 
+solve state [] _             = Just []
+solve state ls@(x : xs) num
+        | isValidMove state x num 
+          && isJust solution = solution 
+        | otherwise          = solve state ls (num + 1)
+    where solution = solve (updateState state x num) xs num
+-- solve state (x : ls) num = takeWhile fun [1..9]
+
+
+isValidMove :: Grid -> (RowNum, ColNum) -> Int -> Bool
+isValidMove ls _ _ = True
+
+updateState :: Grid -> (RowNum, ColNum) -> Int -> Grid
+updateState state point num = map (replace point num) state 
+    where replace point num cell@(sPoint, _)
+            | sPoint == point = (point, Just num)
+            | otherwise       = cell
+
+
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,42 @@
+import Test.HUnit
+import Grid
+import Data.Maybe(isNothing)
+
+-- taken from https://www.websudoku.com/?level=1&set_id=141215464
+testGrid :: [((RowNum, ColNum), Maybe Int)] 
+testGrid = 
+    [
+        ((1, 1), Just 2), ((1, 2), Nothing), ((1, 3), Just 5), ((1, 4), Nothing), ((1, 5), Just 3), ((1, 6), Nothing), ((1, 7), Just 1), ((1, 8), Nothing), ((1, 9), Just 8), 
+        ((2, 1), Nothing), ((2, 2), Nothing), ((2, 3), Just 9), ((2, 4), Just 8), ((2, 5), Nothing), ((2, 6), Nothing), ((2, 7), Nothing), ((2, 8), Just 2), ((2, 9), Nothing), 
+        ((3, 1), Nothing), ((3, 2), Just 8), ((3, 3), Nothing), ((3, 4), Just 1), ((3, 5), Just 7), ((3, 6), Nothing), ((3, 7), Nothing), ((3, 8), Nothing), ((3, 9), Nothing), 
+        ((4, 1), Nothing), ((4, 2), Just 1), ((4, 3), Just 2), ((4, 4), Nothing), ((4, 5), Just 8), ((4, 6), Nothing), ((4, 7), Nothing), ((4, 8), Just 9), ((4, 9), Nothing), 
+        ((5, 1), Just 9), ((5, 2), Nothing), ((5, 3), Just 7), ((5, 4), Nothing), ((5, 5), Just 6), ((5, 6), Nothing), ((5, 7), Just 4), ((5, 8), Nothing), ((5, 9), Just 1), 
+        ((6, 1), Nothing), ((6, 2), Just 6), ((6, 3), Nothing), ((6, 4), Nothing), ((6, 5), Just 4), ((6, 6), Nothing), ((6, 7), Just 2), ((6, 8), Just 3), ((6, 9), Nothing), 
+        ((7, 1), Nothing), ((7, 2), Nothing), ((7, 3), Nothing), ((7, 4), Nothing), ((7, 5), Just 1), ((7, 6), Just 7), ((7, 7), Nothing), ((7, 8), Just 6), ((7, 9), Nothing), 
+        ((8, 1), Nothing), ((8, 2), Just 9), ((8, 3), Nothing), ((8, 4), Nothing), ((8, 5), Nothing), ((8, 6), Just 8), ((8, 7), Just 7), ((8, 8), Nothing), ((8, 9), Nothing), 
+        ((9, 1), Just 7), ((9, 2), Nothing), ((9, 3), Just 3), ((9, 4), Nothing), ((9, 5), Just 9), ((9, 6), Nothing), ((9, 7), Just 8), ((9, 8), Nothing), ((9, 9), Just 4)
+    ]
+
+testEmptyCells :: [(RowNum, ColNum)]
+testEmptyCells = map fst (filter (\x -> isNothing $ snd x) testGrid)
+
+-- inputState :: [((RowNum, ColNum), Maybe Int)] 
+-- inputState = updateState state point num 5
+
+-- expectedOutputState :: [((RowNum, ColNum), Maybe Int)] 
+
+
+test1 = TestCase (assertEqual "for testGrid," testGrid [((1, 2), Just 5)])
+
+-- test2 = TestCase (do (x,y) <- partA 3
+--                      assertEqual "for the first result of partA," 5 x
+--                      b <- partB y
+--                      assertBool ("(partB " ++ show y ++ ") failed") b)
+
+tests = TestList [TestLabel "test1" test1] --, TestLabel "test2" test2]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = 
+    do 
+        results <- runTestTT tests
+        print results
